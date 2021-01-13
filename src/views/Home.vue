1217225e1148ac0050ce91dc68f1a86170e011fe
hdk236p <hdk236p@edacom.co.kr>
2021-01-13 18:24:50 +0900

    test

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <p>{{msg}}</p> -->
+    <Test />
+    <TostPopup />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import Test from '@/components/Test.vue'
+import TostPopup from '@/components/TostPopup.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    //HelloWorld
+    Test,
+    TostPopup
+  },
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
---
+++ vetur.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	projects: [
+		{
+		jsconfig: './jsconfig.json',
+		package: './[folder-name]/package.json',
+		},
+	],
+}
